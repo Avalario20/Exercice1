a3420976362a93498db7259afac6f838bb504813
Avalario20 <luanosamsung@gmail.com>
2024-11-09 16:29:37 +0100

    maj solde

--- script.js
+++ script.js
@@ -1,43 +1,47 @@
-let tauxInteret = 0.03;
-const solde = 1000;
-let depot = 200;
-let retrait = 150;
+const tauxInteret = 0.03;
+let solde = 1000;
+const depot = 200;
+const retrait = 150;
 
-let nouveauDepot = 500;
-let nouveauRetrait = 800;
+const nouveauDepot = 500;
+const nouveauRetrait = 800;
 
-const historique = [];
+let historique = [];
 
-depotArgent(solde, depot);
-retraitArgent(solde, retrait);
-calculInteret(solde, tauxInteret);
 
-depotArgent(solde, nouveauDepot);
-retraitArgent(solde, nouveauRetrait);
-calculInteret(solde, tauxInteret);
+solde = depotArgent(solde, depot);
+solde = retraitArgent(solde, retrait);
+solde = calculInteret(solde, tauxInteret);
+
+solde = depotArgent(solde, nouveauDepot);
+solde = retraitArgent(solde, nouveauRetrait);
+solde = calculInteret(solde, tauxInteret);
 
 console.table(historique);
 
-function depotArgent(solde, depot){
-    solde += depot;
 
-    console.log("Vous avez déposé " +depot+ " euros. Nouveau solde : " +solde+ " euros.");
-    historique.push("Dépôt de " +depot+ " euros. Solde : " +solde+ " euros.");
+function depotArgent(solde, depot) {
+    solde = solde + depot;
+    console.log("Vous avez déposé " + depot + " euros. Nouveau solde : " + solde + " euros.");
+    historique.push("Dépôt de " + depot + " euros. Solde : " + solde + " euros.");
+    return solde;
 }
 
-function retraitArgent(solde, retrait){
+function retraitArgent(solde, retrait) {
     if (solde >= retrait) {
-        solde -= retrait;
-        console.log("Vous avez retiré " +retrait+ " euros. Nouveau solde : " +solde+ " euros.");
-        historique.push("Retrait de " +retrait+ " euros. Solde : " +solde+ " euros.");
-    }else{
+        solde = solde - retrait;
+        console.log("Vous avez retiré " + retrait + " euros. Nouveau solde : " + solde + " euros.");
+        historique.push("Retrait de " + retrait + " euros. Solde : " + solde + " euros.");
+    } else {
         console.log("Solde insuffisant pour effectuer ce retrait.");
-        historique.push("Tentative de retrait de " +retrait+ " euros. Solde insuffisant. Solde : " +solde+ " euros.");
+        historique.push("Tentative de retrait de " + retrait + " euros. Solde insuffisant. Solde : " + solde + " euros.");
     }
+    return solde;
 }
 
-function calculInteret(solde, tauxInteret){
+function calculInteret(solde, tauxInteret) {
     solde += solde * tauxInteret;
-    console.log("Intérêts annuels de 3% ajoutés. Nouveau solde : " +solde+ " euros.");
-    historique.push("Intérêts annuels de 3% ajoutés. Solde : " +solde+ " euros.");
+    console.log("Intérêts annuels de 3% ajoutés. Nouveau solde : " + solde + " euros.");
+    historique.push("Intérêts annuels de 3% ajoutés. Solde : " + solde + " euros.");
+    return solde;
 }
